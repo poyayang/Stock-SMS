3ee244a81f25f3a1e5e2749f787ae09bcae4c78d
PoYa <poya.yang@icloud.com>
2023-09-11 22:55:49 +0100

    To edit decimal of the numbers
    Using f-string to get the first two digits of decimal

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def sms():
     for index, row in info_needed.iterrows():
         data_dict={'Date': index.strftime('%Y-%m-%d'), 
                    'Ticker': message_body.upper(), 
-                   'Open': row['Open'],
-                   'High': row['High'],
-                   'Low': row['Low'],
-                   'Close': row['Close']}
+                   'Open': f"{row['Open']:.2f}",   
+                   'High': f"{row['High']:.2f}",
+                   'Low': f"{row['Low']:.2f}",
+                   'Close': f"{row['Close']:.2f}"} # f"{a:.2f}" <----- to get first 2 decimal by using f-string
         data_list.append(data_dict)
     response_content='Hello {}, here is the stock info:\n'.format(number)
     for data in data_list:
