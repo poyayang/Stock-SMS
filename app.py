910222114aedc6fe17a7134d9dfac50f2721f97b
PoYa <poya.yang@icloud.com>
2023-10-27 22:24:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 import yfinance as yf
 
 # import pandas as pd
-from datetime import datetime
+from datetime import date
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def sms() -> str:
     stock_symbol = request.form["Body"]
     stock_prices = get_stock_price(stock_symbol)
     message = message_structure(
-        datetime.today(),
+        date.today(),
         stock_symbol,
         stock_prices["Open"],
         stock_prices["Close"],
@@ -31,7 +31,7 @@ def get_stock_price(company: str) -> dict:
     company_hist = tickers.history(period="1d")
     prices = company_hist[["Open", "High", "Low", "Close"]]
     price = {
-        "Date": datetime.now().strftime("%Y-%m-%d"),
+        "Date": date.today().strftime("%y-%m-%d"),
         "Open": prices["Open"].values[0],
         "Close": prices["Close"].values[0],
         "High": prices["High"].values[0],
@@ -41,7 +41,7 @@ def get_stock_price(company: str) -> dict:
 
 
 def message_structure(
-    date: datetime, ticker: str, open: float, close: float, high: float, low: float
+    date: date, ticker: str, open: float, close: float, high: float, low: float
 ) -> str:
     strdate = str(date.strftime("%y-%m-%d"))
     message = f"Date:{strdate}\nCompany:{ticker}\nOpen:{open}\nClose:{close}\nHigh:{high}\nLow:{low}"
